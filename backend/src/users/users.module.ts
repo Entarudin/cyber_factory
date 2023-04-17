ac26f2cf82cea603cbc98182ae95b75e2c5f4322
Entarudin <riazanov@sfedu.ru>
2023-04-17 13:27:36 +0300

    update imports in userepository

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserEntity } from './entity/user.entity';
+import { UserEntity } from './dao/entity/user.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserEntity])],
