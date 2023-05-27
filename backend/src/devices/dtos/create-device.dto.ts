0cb637b79b4acd0abb6015b0aafe66d5253a8950
Entarudin <riazanov@sfedu.ru>
2023-05-27 13:50:22 +0300

    [BE] Added devices Dto

---
+++ backend/src/devices/dtos/create-device.dto.ts
@@ -0,0 +1,50 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsDefined,
+  IsNumber,
+  IsIP,
+  IsString,
+  IsMACAddress,
+} from 'class-validator';
+
+export class CreateDeviceDto {
+  @ApiProperty({
+    type: String,
+    required: true,
+  })
+  @IsString()
+  @IsDefined()
+  public readonly name: string;
+
+  @ApiProperty({
+    type: String,
+    required: true,
+  })
+  @IsIP()
+  @IsDefined()
+  public readonly ipAddress: string;
+
+  @ApiProperty({
+    type: String,
+    required: true,
+  })
+  @IsMACAddress()
+  @IsDefined()
+  public readonly macAddress: string;
+
+  @ApiProperty({
+    type: String,
+    required: true,
+  })
+  @IsString()
+  @IsDefined()
+  public readonly networkInterface: string;
+
+  @ApiProperty({
+    type: Number,
+    required: true,
+  })
+  @IsNumber()
+  @IsDefined()
+  public readonly cyberPhysicalSystemId: number;
+}
